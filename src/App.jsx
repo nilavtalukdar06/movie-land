d9346ae034b202762beb17a67e6d0414271bed42
Nilav <nilavtalukdar06@gmail.com>
2025-03-01 15:13:02 +0530

    Added movies

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,6 @@ const API_KEY = import.meta.env.VITE_API_KEY;
 const ID = import.meta.env.VITE_OMDB_ID;
 const URL = `http://www.omdbapi.com?apiKey=${API_KEY}&i=${ID}`;
 
-const movie1 = {
-  Title: "The Avengers",
-  Year: "2012",
-  imdbID: "tt0848228",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BNGE0YTVjNzUtNzJjOS00NGNlLTgxMzctZTY4YTE1Y2Y1ZTU4XkEyXkFqcGc@._V1_SX300.jpg",
-};
-
 const App = () => {
   const [movies, setMovies] = useState([]);
 
@@ -50,7 +41,9 @@ const App = () => {
 
       {movies.length > 0 ? (
         <div className="container">
-          <MovieCard movie1={movie1} />
+          {movies.map((movie, index) => (
+            <MovieCard movie={movie} key={index} />
+          ))}
         </div>
       ) : (
         <div className="empty">
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 
-const MovieCard = ({ movie1 }) => {
+const MovieCard = ({ movie }) => {
   return (
     <div className="movie">
       <div>
-        <p>{movie1.Year}</p>
+        <p>{movie.Year}</p>
       </div>
 
       <div>
         <img
-          src={movie1.Poster !== "N/A" ? movie1.Poster : "/notfound.avif"}
-          alt={movie1.Title}
+          src={movie.Poster !== "N/A" ? movie.Poster : "/notfound.avif"}
+          alt={movie.Title}
           className="poster"
         />
       </div>
 
       <div>
-        <span>{movie1.Type}</span>
-        <h3>{movie1.Title}</h3>
+        <span>{movie.Type}</span>
+        <h3>{movie.Title}</h3>
       </div>
     </div>
   );
