d2cdb8be9ef9ac87b632e7b98af1a94cb9b46878
Nilav <nilavtalukdar06@gmail.com>
2025-03-01 13:59:49 +0530

    Added CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
 import React, { useEffect } from "react";
+import "./app.css";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 const ID = import.meta.env.VITE_OMDB_ID;
 const URL = `http://www.omdbapi.com?apiKey=${API_KEY}&i=${ID}`;
 
+const fetchData = async (title) => {
+  try {
+    const response = await fetch(`${URL}&s=${title}`, { method: "GET" });
+    if (!response.ok) {
+      console.log("Some error occurred");
+      return;
+    }
+    const data = await response.json();
+    console.log(data.Search);
+  } catch (error) {
+    console.log(`Error fetching data, error: ${error}`);
+  }
+};
+
 const App = () => {
   useEffect(() => {
-    const fetchData = async (title) => {
-      try {
-        const response = await fetch(`${URL}&s=${title}`, { method: "GET" });
-        if (!response.ok) {
-          console.log("Some error occurred");
-          return;
-        }
-        const data = await response.json();
-        console.log(data);
-      } catch (error) {
-        console.log(`Error fetching data, error: ${error}`);
-      }
-    };
-
-    fetchData();
+    fetchData("Avengers");
   }, []);
 
-  return <div>MovieLand</div>;
+  return <section></section>;
 };
 
 export default App;
